a71893b68c77aac0d6ba0cb1574216683016a81b
Alejandro Caravaca Puchades <59510094+acpuchades@users.noreply.github.com>
2025-02-07 00:15:14 +0000

    Add spliceAI predictive score plots to report files

--- src/summary-stats.r
+++ src/summary-stats.r
@@ -2,11 +2,13 @@ library(dplyr)
 library(readr)
 library(stringr)
 library(forcats)
+library(tidyr)
 
 library(ggplot2)
 library(ggvenn)
 library(scales)
 library(patchwork)
+library(GGally)
 
 as_chromosome <- function(x) {
   factor(x, levels = c(1:22, "X", "Y", "MT"))
@@ -24,13 +26,49 @@ variant.stats <- read_tsv("output/analysis-report/variant-stats.tsv") |>
 variants_per_locus <- variant.stats |>
   slice_sample(n = 1, by = c(locus, "REF", "ALT"))
 
+variant.scores <- variants_per_locus |>
+  select(locus, REF, ALT, ends_with("_rankscore"), matches("SpliceAI_pred_DS_[AD][GL]")) |>
+  pivot_longer(-c(locus, REF, ALT), names_to = "score", values_to = "value", values_transform = as.numeric) |>
+  mutate(score = score |>
+    str_replace_all("_converted", "") |>
+    str_replace_all("_raw", "") |>
+    str_replace_all("_rankscore$", ""))
+
+variant.scores |>
+  filter(!str_starts(score, "SpliceAI_")) |>
+  ggplot(aes(value)) +
+  geom_histogram() +
+  facet_wrap(~score, scales = "free") +
+  scale_x_continuous(limits = c(0, 1)) +
+  labs(title = "Impact prediction scores", x = "Rank score", y = "Count") +
+  theme(plot.title = element_text(hjust = 0.5))
+ggsave("output/analysis-report/variants-predicted.png")
+
+variant.scores |>
+  filter(str_starts(score, "SpliceAI_")) |>
+  mutate(score = score |>
+    str_replace("SpliceAI_pred_DS_", "") |>
+    case_match(
+      "AG" ~ "Acceptor Gain",
+      "AL" ~ "Acceptor Loss",
+      "DG" ~ "Donor Gain",
+      "DL" ~ "Donor Loss"
+    )) |>
+  ggplot(aes(value)) +
+  geom_histogram() +
+  facet_wrap(~score) +
+  scale_x_continuous(trans = "log10") +
+  labs(title = "SpliceAI delta scores", x = "Delta score", y = "Count") +
+  theme(plot.title = element_text(hjust = 0.5))
+ggsave("output/analysis-report/variants-spliceai.png")
+
 af_plot <- ggplot(variants_per_locus, aes(gno_af_all)) +
   geom_histogram() +
   scale_x_continuous(labels = label_percent())
 
 af_plot_log10 <- ggplot(variants_per_locus, aes(gno_af_all)) +
   geom_histogram() +
-  scale_x_continuous(name = "log(gno_af_all)", labels = label_percent(), trans = "log10")
+  scale_x_continuous(name = "gno_af_all", labels = label_percent(), trans = "log10")
 
 (af_plot | af_plot_log10) + plot_annotation(
   title = "Allele frequency of variants according to gnomAD genomes",
